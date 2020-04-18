b48dc69dd071362a37dad6abf0253496444f7cb0
Jose Luna <jmlunamugica@mac.com>
2020-04-17 19:36:21 -0500

    Complete airbnb challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  flat = Flat.new(
+    name:    "#{Faker::FunnyName.two_word_name}'s Home",
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::Quote.yoda,
+    price_per_night: Faker::Number.between(from: 50, to: 1000),
+    number_of_guests: Faker::Number.between(from: 1, to: 15)
+  )
+  flat.save!
+end
